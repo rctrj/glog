8522baf97e11ae7bdcd336cf721b7c8e9a591825
Rachit Raj <rachitraj2904@gmail.com>
2022-08-29 12:00:08 +0530

    add additional initializer for fmtcore config

--- fmtcore/logger.go
+++ fmtcore/logger.go
@@ -7,9 +7,16 @@ type Config struct {
 }
 
 func NewConfig(levelStr, stripLevelStr string, prettyPrint, displayPrefix bool) Config {
-	return Config{
-		log.NewConfig(levelStr, stripLevelStr, prettyPrint, displayPrefix),
-	}
+	return NewConfigFromLogConfig(log.NewConfig(levelStr, stripLevelStr, prettyPrint, displayPrefix))
+}
+
+func NewConfigFromLogConfig(in log.Config) Config {
+	return Config{log.Config{
+		Level:         in.Level,
+		StripLevel:    in.StripLevel,
+		PrettyPrint:   in.PrettyPrint,
+		DisplayPrefix: in.DisplayPrefix,
+	}}
 }
 
 func New(conf Config) *log.Logger {
